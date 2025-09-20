0cb52c2d756dc6df21f4210ae9539916eac50150
Balaji4689 <balajireddymaddula123@gmail.com>
2025-09-20 18:04:00 +0530

    Update Api.jsx

--- SocialAi/SocilaAi/src/helper/Api.jsx
+++ SocialAi/SocilaAi/src/helper/Api.jsx
@@ -2,5 +2,9 @@
 
 
 
-export const API_path = "http://localhost:2000";
+// export const API_path = "http://localhost:2000";
+
+
+
+export const API_path = "https://socialai-application.onrender.com"
 
